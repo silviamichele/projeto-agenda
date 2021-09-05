96b16429359f39712b3ab019cd3b4d1ba7813e7a
Silvia Michele <silvia.teles.br@gmail.com>
2021-09-04 21:19:56 -0300

    template para cadastro de evento

--- core/views.py
+++ core/views.py
@@ -15,6 +15,24 @@ def lista_eventos(request):
 	eventos = Eventos.objects.filter(usuario=user)
 	return render(request, 'agenda.html', {'eventos':eventos})
 
+@login_required(login_url='/login/')
+def evento(request):
+	return render(request, 'evento.html')
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+	if request.POST:
+		titulo = request.POST.get('titulo')
+		data_evento = request.POST.get('data_evento')
+		descricao = request.POST.get('descricao')
+		usuario = request.user
+		Eventos.objects.create(titulo=titulo, 
+			data_evento=data_evento, 
+			descricao=descricao,
+			usuario=usuario)
+
+	return redirect('/')
+
 def index(request):
 	return redirect('/agenda')
 
